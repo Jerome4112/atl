31dcccd25d7dd7645dd14500a75d96ef6c799f75
Jerome4112 <jerome1997@gmail.com>
2023-09-12 18:49:33 +0200

    new repository

--- routes/employee.py
+++ routes/employee.py
@@ -17,14 +17,14 @@ def read_employees(skip: int = 0, limit: int = 100, db: Session = Depends(get_db
     employees = get_employees(db, skip=skip, limit=limit)
     return employees
 
-
+"""""
 @router.get("/{employee_id}", response_model=Employee, tags=["Employee"])
 def read_employee(employee_id: int, db: Session = Depends(get_db)):
     db_employee = get_employee(db, employee_id=employee_id)
     if db_employee is None:
         raise HTTPException(status_code=404, detail="Employee not found")
     return db_employee
-
+"""
 @router.post("/{customer_id}", response_model=Employee, tags=["Employee"])
 def create_employee( customer_id: int,employee: EmployeeCreate, db: Session = Depends(get_db)):
     db_employee = get_employee(db, employee.id)
